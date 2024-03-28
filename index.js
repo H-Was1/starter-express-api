5752c7574c6bc38c47932dced9b62be60e2abcca
H Was1 <hammadwasi.b@gmail.com>
2024-03-28 04:51:52 -0700

    core+sparticuz

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const  scrapeAqi  = require("./lib/scraper");
+const app = express();
+app.get("/", scrapeAqi);
+app.listen(process.env.PORT || 3000);
