b6d2fb088634483205a39b078a23da72b69b0b84
H Was1 <hammadwasi.b@gmail.com>
2024-03-28 07:21:50 -0700

    lambda

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const  scrapeAqi  = require("./lib/scraper");
+const scrape = require("./lib/scraper");
 const app = express();
-app.get("/", scrapeAqi);
+app.get("/", scrape);
 app.listen(process.env.PORT || 3000);
